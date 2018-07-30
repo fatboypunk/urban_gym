755dd4db9063d85f868b634bf4d0ed8b7ad39512
Marcel Horlings <marcel.horlings@gmail.com>
2018-07-30 15:26:45 +0200

    add pictures of gyms and make a navication link

--- config.rb
+++ config.rb
@@ -42,6 +42,7 @@ page '/*.txt', layout: false
 
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
+#
 
 configure :build do
   activate :minify_css
